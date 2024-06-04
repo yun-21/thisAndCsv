e921a75536aa50925ed3759f79dd8f36a1363d1d
shinjiyun <ranji12@naver.com>
2024-06-04 10:56:25 +0900

    덧셈 밖에서 해보기

--- work-2.js
+++ work-2.js
@@ -11,7 +11,17 @@ class WorkTwo{
       throw new Error("a는 숫자만 입력할 수 있습니다.")
     }
   }
+
+  set b(value){
+    if(typeof(value)==="number"){
+      return this._b = value;
+    }else{
+      throw new Error("b는 숫자만 입력할 수 있습니다.")
+    }
+  }
 }
 
-let test = new WorkTwo("Dd",2);
+let test = new WorkTwo(1,2);
 console.log(test);
+console.dir(test);
+console.log(test._a+test._b);
