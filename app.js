163530c4af9d13fcb7cb69fc815ed99b20c32f59
jiyun <ranji12@naver.com>
2024-06-04 22:04:03 +0900

    csv데이터를 읽어와서 JSON으로 변환시켜 json파일 작성완료

--- app.js
+++ app.js
@@ -3,8 +3,29 @@ const fs = require('fs');
 const a = fs.readFileSync('./basicData.csv', "utf-8")
 
 const b = a.split('\r\n')
+
 const array = [];
 for(let key in b){
   array.push(b[key].split(','))
 }
-console.log(array);
+
+//csv데이터를 넣어줄 object 객체 초기화
+const object = {};
+
+//값으로 넣어줄 객체 초기화
+const value = {};
+
+//객체값을 가진 지역키
+object[array[0][0]] = value
+
+for (let i = 1; i < array.length; i++) {
+  //값으로 넣어줄 객체 초기화
+  const value2 = {}
+  //객체값을 가진 지역명키
+  value[array[i][0]] = value2
+  for (let j = 1; j < array[0].length; j++) {   
+    //각 연도를 키로 두고 그 연도에 해당되는 값을 프로퍼티 값으로 줬음 
+    value2[array[0][j]+"년"] = array[i][j]+"개"
+  }
+}
+fs.writeFileSync("./csvDataJson.json",JSON.stringify(object, null, 2))
