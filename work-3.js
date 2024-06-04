430613182c14ac3b4cae264432f36c254a789817
shinjiyun <ranji12@naver.com>
2024-06-04 11:47:58 +0900

    this 헷갈릴때 보기

--- work-3.js
+++ work-3.js
@@ -4,3 +4,15 @@ function workThree(a,b){
   return a+b
 }
 console.log(workThree(1,2));
+
+let itsMe = {
+  name : "신지윤",
+  age : 25,
+  home : "대전",
+  test : this,
+  sentence : function(){
+    return this.name + "입니다.";
+  }
+}
+console.log("객체에서 보는 this : ",itsMe);
+console.log("메서드로 보는 this : ", itsMe.sentence())
