521763d0b77589747c8a27ade1fad88696174a93
shinjiyun <ranji12@naver.com>
2024-06-04 15:04:50 +0900

    전기자동차 충전기 현황정보파일 한줄 삭제

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const fs = require('fs');
 
-const a = fs.readFileSync('./basicData.csv',"utf-8")
-console.log(a)
+const a = fs.readFileSync('./basicData.csv', "utf-8")
+
+const b = a.split('\r\n')
+console.log(b[17])
+
+console.log(JSON.stringify(a));
