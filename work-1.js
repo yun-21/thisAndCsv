2e3df24e522aad90d814ae8b74131b1d539bdc62
shinjiyun <ranji12@naver.com>
2024-06-04 10:36:27 +0900

    조건식에 else 추가

--- work-1.js
+++ work-1.js
@@ -2,5 +2,8 @@ function workOne(a,b){
   if(typeof(a)==="number" && typeof(b)==="number"){
     let local = a + b;
     return local
+  }else{
+    console.error('숫자만 적어주세요');
   }
 }
+console.log(workOne(1,12))
