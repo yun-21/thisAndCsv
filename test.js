b6e88970256bd7af5cb35ed79eb6f4aea9b28ab3
jiyun <ranji12@naver.com>
2024-06-04 21:40:43 +0900

    csv데이터 객체에 넣기 테스트 성공

--- test.js
+++ test.js
@@ -1,26 +1,23 @@
 const list = [
-  ["지역"],
-  ["대전", "2", "3"],
-  ["서울", "3", "4"]
+  ["지역", "2016", "2017", "2018", "2019", "2020", "2021", "2022"],
+  ["강원도", "25", "163", "236", "253", "347", "377", "468"],
+  ["경기도", "140", "735", "1639", "1734", "2023", "2151", "2268"]
 ]
+//csv를 객체화 시켜서 넣을 변수 초기화
 const object = {};
 
-const local = list[0]
-const daejeon = list[1][0]
-const a = {}
-object[local] = a
-a[daejeon] = list[1][1]
-// console.log(object)
+//값으로 넣어줄 객체 초기화
+const value = {};
+object[list[0][0]] = value
 
 
 for (let i = 1; i < list.length; i++) {
-  var name = list[i][0];
-  const num = list[i].slice(1)
-  const content = num[0].split(",")
-  const ddd = {}
-  a[name] = ddd
-  ddd[num] = num[i]
-  console.log(content)
-  // console.log(num)
+  const value2 = {}
+  value[list[i][0]] = value2
+  for (let j = 1; j < list.length; j++) {
+    const year = list[0][j]
+    value2[year] = 1
+  }
 }
+console.log(JSON.stringify(object, null, 2))
 // list[1].shift(); // 배열 첫번째요소 날림
