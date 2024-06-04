adc140d0e86d35f409663743887641f3afd4c341
shinjiyun <ranji12@naver.com>
2024-06-04 10:51:27 +0900

    setter만들어서 에러처리까지 진행했음

--- work-2.js
+++ work-2.js
@@ -3,7 +3,15 @@ class WorkTwo{
     this.a = a;
     this.b = b;
   }
+
+  set a(value){
+    if(typeof(value)==="number"){
+      return this._a = value;
+    }else{
+      throw new Error("a는 숫자만 입력할 수 있습니다.")
+    }
+  }
 }
 
-let test = new WorkTwo(1,2);
+let test = new WorkTwo("Dd",2);
 console.log(test);
