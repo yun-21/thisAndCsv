132222426d7d6f6b1c720c2b1a801a08b1ab04c4
shinjiyun <ranji12@naver.com>
2024-06-04 15:25:24 +0900

    지역 데이터 추출

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 const a = fs.readFileSync('./basicData.csv', "utf-8")
 
 const b = a.split('\r\n')
-console.log(b[17])
-
-console.log(JSON.stringify(a));
+for(let key in b){
+  console.log(b[key].split(',')[0])
+}
