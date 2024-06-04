17dab5a0750011ab18f2b567b4317ce5aa919cc9
shinjiyun <ranji12@naver.com>
2024-06-04 14:28:10 +0900

    정적인 생성자함수 만들었음

--- class.js
+++ class.js
@@ -1,1 +1,14 @@
-console.log('class.js')
+class ItsMe{
+  constructor(){
+    this.name ="신지윤",
+    this.age = 25,
+    this.home = '대전'
+  }
+  sentence(){
+    return this.name + "입니다";
+  }
+}
+
+let itsMe = new ItsMe();
+console.log(itsMe);
+console.log(itsMe.sentence());
