a0a20ba17e27e978d41582a440b53ebc35e01b61
shinjiyun <ranji12@naver.com>
2024-06-05 09:26:21 +0900

    함수에 넣어서 작동해보기

--- app.js
+++ app.js
@@ -2,30 +2,34 @@ const fs = require('fs');
 
 const a = fs.readFileSync('./basicData.csv', "utf-8")
 
-const b = a.split('\r\n')
-
-const array = [];
-for(let key in b){
-  array.push(b[key].split(','))
-}
-
-//csv데이터를 넣어줄 object 객체 초기화
-const object = {};
-
-//값으로 넣어줄 객체 초기화
-const value = {};
-
-//객체값을 가진 지역키
-object[array[0][0]] = value
-
-for (let i = 1; i < array.length; i++) {
-  //값으로 넣어줄 객체 초기화
-  const value2 = {}
-  //객체값을 가진 지역명키
-  value[array[i][0]] = value2
-  for (let j = 1; j < array[0].length; j++) {   
-    //각 연도를 키로 두고 그 연도에 해당되는 값을 프로퍼티 값으로 줬음 
-    value2[array[0][j]+"년"] = array[i][j]+"개"
+function csvTest(a,c){
+  
+  const b = a.split('\r\n')
+  
+  const array = [];
+  for(let key in b){
+    array.push(b[key].split(','))
   }
+  
+  //csv데이터를 넣어줄 object 객체 초기화
+  const object = {};
+  
+  //값으로 넣어줄 객체 초기화
+  const value = {};
+  
+  //객체값을 가진 지역키
+  object[array[0][0]] = value
+  
+  for (let i = 1; i < array.length; i++) {
+    //값으로 넣어줄 객체 초기화
+    const value2 = {}
+    //객체값을 가진 지역명키
+    value[array[i][0]] = value2
+    for (let j = 1; j < array[0].length; j++) {   
+      //각 연도를 키로 두고 그 연도에 해당되는 값을 프로퍼티 값으로 줬음 
+      value2[array[0][j]+"년"] = array[i][j]+"개"
+    }
+  }
+  fs.writeFileSync(`${c}`,JSON.stringify(object, null, 2))
 }
-fs.writeFileSync("./csvDataJson.json",JSON.stringify(object, null, 2))
+csvTest(a,'./a.json')
