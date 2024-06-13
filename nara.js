740ac26eec3214f9bced621bf0c83af818f31b89
jiyun <ranji12@naver.com>
2024-06-13 23:07:56 +0900

    연도빼기

--- nara.js
+++ nara.js
@@ -1,21 +1,39 @@
 const fs = require('fs')
-const csvData = fs.readFileSync("Nara.csv","utf-8")
+const csvData = fs.readFileSync("Nara.csv", "utf-8")
 
-const arrayCsv = (callback) =>{
+const arrayCsv = (callback) => {
   const arrCsvData = csvData.split('\r\n')
-  callback(arrCsvData,arrData)
+  callback(arrCsvData, arrData)
 }
 
-const arrData= (data,callback)=>{
-  const arr =[];
-  for(let key in data){
+const arrData = (data, callback) => {
+  const arr = [];
+  for (let key in data) {
     arr.push(data[key].split(","));
   }
-  callback(arr,filterData);
+  callback(arr, filterData);
 }
 
-const filterData = (arr)=>{
-  const result = arr[0].filter((word)=>word.includes("연도"))
+const filterData = (arr) => {
+  const year = arr[0].filter((word) => word.includes("연도"))
+  const deletes = (year) => {
+    const array = [];
+    for (let key in year) {
+      array.push(year[key].split(" "))
+    }
+    function inner(array) {
+      array.forEach((element) => {
+        element.splice(0, 1)
+      })
+      return array
+    }
+    inner(array)
+    return array
+  }
+  return deletes(year)
 }
 
-arrayCsv((data,callback)=>(callback(data,filterData)));
+
+arrayCsv((data, callback) => (callback(data, filterData)));
+
+
